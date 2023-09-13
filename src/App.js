627925369e144ed3b5841b795fb177e08f9027df
Lumala Brian <lumalabravo@gmail.com>
2023-09-13 19:19:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className=' overflow-hidden' >
-      <div className=' m-48 bg-red-300 p-48'>Hello</div>
+    <div className=" overflow-hidden">
+      <div className=" m-48 bg-red-300 p-48 justify-normal">Hello</div>
     </div>
   );
 }
